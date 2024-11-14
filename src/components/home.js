0e7fd7e2f38d2971a5db35af061a620785db3a85
liquid-krn <alagoke@gmail.com>
2024-11-14 14:45:34 +0100

    working on  desktop page

--- src/components/home.js
+++ src/components/home.js
@@ -29,10 +29,15 @@ function Home() {
         backgroundImage: `url("./images/2.jpg")`
     }}
 >
+<div className="homepage">
+        <div className="text" style={{backgroundColor:"#21252e", padding:"10px", borderRadius:"15px"}}>
+            <h1>Welcome to cresendo</h1>
+        </div>
         <div className="hompagebuttons">
             <Button onClick={handleSignupClick} className="btn btn-primary mt-3" type="button" text={isClicked ? "Loading" : "Signup"} />
             <Button onClick={handleLoginClick} className="btn btn-light mt-3" type="button" text={isClicked ? "Loading" : "Login"} />
         </div>
+</div>        
     <Footer /> 
     </div>
 </>
